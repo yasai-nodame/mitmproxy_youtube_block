b6653a5ff0914cb08e5310289a4864ce73eb1517
yasai-nodame <yasaisecret1@gmail.com>
2024-07-18 19:30:58 +0900

    いらないcooke headerを削除して、挿入

--- test_addblock.py
+++ test_addblock.py
@@ -36,7 +36,7 @@ def requestheaders(flow: http.HTTPFlow):
 
             ctx.log.info(f'new_cookies: {divide_cookies}') # new_cookiesにcookiesのheaderとその値が入っている。
                         
-            for i in range(len(divide_cookies) - 1, -1, -1):
+            for i in range(len(divide_cookies) - 1, -1, -1): # 逆順に要素を取得していく理由は、順方向だと、削除したときにインデックスがずれるのを防ぐため。
                 for target_cookie in target_cookies:
                     if divide_cookies[i].strip() == target_cookie:
                         del divide_cookies[i]
@@ -44,9 +44,16 @@ def requestheaders(flow: http.HTTPFlow):
                             del divide_cookies[i]
             
             # 前の要素と次の要素をcookie header形式に変換　つまり、 name=valueの形式にする。
+            cookies_str = ''
+            for i in range(0, len(divide_cookies), 2): # 0~リストの長さlen(divide_cookies)を、2ずつ飛ばして回していく。step
+                if i + 1 < len(divide_cookies): # list index out of range 防止で i + 1がリストの長さ以下だった場合 true:
+                    cookies_str += f'{divide_cookies[i]}={divide_cookies[i + 1]}; ' # cookie headerのように ;をつける。
+
+            # 最後のセミコロンとスペースを削除
+            if cookies_str.endswith('; '): # cookies_str.endwith(';') つまり、cookies_strの最後が;の場合 true:
+                cookies_str = cookies_str[:-2] # セミコロンと空白がついているため [:-2]のスライスで削除。 つまり、cookies_strの0から-2までの部分文字列を取得。
             
-            
-            
+            ctx.log.info(f'cookies_str: {cookies_str}')
             
     """
     ['aaa=aaa', 'bbb=bbb', 'ccc=ccc']となったとき、 flow.request.headers.get('cookie', '').split('=')
@@ -62,6 +69,40 @@ def requestheaders(flow: http.HTTPFlow):
     書き方としては、 list = [1,2,3,4,5,6] とある時、 
     append()の場合は、単一なので、 list.append(10)で、 list = [1,2,3,4,5,6,10]となる。
     extend()の場合は、複数なので、 list.extend([10,11]) とすると、 list = [1,2,3,4,5,6,10,11]となる。
+    
+    
+    次に、for i in range(len(divide_cookies) - 1, -1, -1): # 逆順に要素を取得していく理由は、順方向だと、削除したときにインデックスがずれるのを防ぐため。
+                for target_cookie in target_cookies:
+                    if divide_cookies[i].strip() == target_cookie:
+                        del divide_cookies[i]
+                        if i < len(divide_cookies):
+                            del divide_cookies[i]のコードの説明をする。
+        
+        divide_cookies = ['PREF', 'tz=Asia.Tokyo&f5=30000&f7=100&f4=4000000', 'HSID', 'value1', 'SSID', 'value2'] とする。
+        
+        for i in range(len(divide_cookies) - 1, -1, -1): 
+        これはdivide_cookiesの要素を逆順にする(len(divide_cookies) - 1)
+        逆順にした状態の最後の要素のことを指す(-1), この場合だと、逆順にした状態の最後の要素は0を指す。つまり'PREF'
+        stepで、1回ずつ逆順に回していく(-1)
+        
+        target_cookie in target_cookies:
+        これでtarget_cookiesの要素を回していく。
+        
+        divide_cookies[5].strip() == target_cookie: これはFalseなので、また回す。
+        divide_cookies[4].strip() == target_cookie: これはTrueなので、削除する。
+        divide_cookies = ['PREF', 'tz=Asia.Tokyo&f5=30000&f7=100&f4=4000000', 'HSID', 'value1', 'SSID', 'value2']
+        これから、
+        divide_cookies = ['PREF', 'tz=Asia.Tokyo&f5=30000&f7=100&f4=4000000', 'HSID', 'value1', 'value2']
+        'SSID'を消した状態。len(divide_cookies)は、5のまま。なぜならまだif文が続いていて、回していないから。
+        
+        if i < len(divide_cookies): 
+        この len(divide_cookies)は、この処理が終われば更新されるように設計されている。
+        今は if 4 < 5でTrue:になる。
+        なので、del divide_cookies[4]になり、
+        divide_cookies = ['PREF', 'tz=Asia.Tokyo&f5=30000&f7=100&f4=4000000', 'HSID', 'value1', 'value2']
+        から、
+        divide_cookies = ['PREF', 'tz=Asia.Tokyo&f5=30000&f7=100&f4=4000000', 'HSID', 'value1']
+        になる。
     """
 
 # def responseheaders(flow: http.HTTPFlow):
